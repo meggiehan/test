d8b5f952537fb0547cfd84d249ce8ad7e3ce4f0a
jjskay <404784102@qq.com>
2016-08-29 23:11:17 +0800

    fixed bug for can not go my center.

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ import nativeEvent from '../utils/nativeEvent';
 
 const configs = {
     version: '0.1.8',
-    debug: true,
+    debug: false,
     url: nativeEvent['getAPi']() || 'http://api.test.yudada.com/',
     // url: '://192.168.20.109:8080/',
     backgroundImgUrl: './img/app_icon_108.png',
