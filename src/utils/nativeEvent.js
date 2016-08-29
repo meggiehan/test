c64dd1bd7d0b84e32a450a588eb8d9ff6ed8947c
jjskay <404784102@qq.com>
2016-08-29 11:37:49 +0800

    fixed issue: #134.

--- src/utils/nativeEvent.js
+++ src/utils/nativeEvent.js
@@ -1,7 +1,9 @@
 import config from '../config';
+import framework7 from '../js/lib/framework7';
 
+window.currentDevice = new framework7()['device'];
 class CustomClass {
-    
+
     //The third party statistics.
     apiCount(id) {
         const { ios, android } = window.currentDevice;
@@ -17,7 +19,7 @@ class CustomClass {
     }
 
     //choose address
-    eventChooseAddress(type){
+    eventChooseAddress(type) {
         //pageType:0:release page  1:mycenter page
         const { ios, android } = window.currentDevice;
         ios && JS_ChooseAddress(type);
@@ -25,56 +27,56 @@ class CustomClass {
     }
 
     //get current address.
-    getAddress(){
+    getAddress() {
         const { ios, android } = window.currentDevice;
         ios && JS_LocationOfDevice();
         android && window.yudada.JS_LocationOfDevice();
     }
 
     //select pic
-    postPic(mark, id){
+    postPic(mark, id) {
         const { ios, android } = window.currentDevice;
         ios && JS_PictureSeletor(mark, id);
         android && window.yudada.JS_PictureSeletor(mark, "上传照片", id);
     }
 
     //get api from native.
-    getApi(){
+    getApi() {
         const { ios, android } = window.currentDevice;
         ios && JS_BaseUrl();
         android && window.yudada.JS_BaseUrl();
     }
 
     //cat pic
-    catPic(url){
+    catPic(url) {
         const { ios, android } = window.currentDevice;
         ios && JS_ShowOriginalImg(url);
         android && window.yudada.JS_ShowOriginalImg(url);
     }
 
     //weixin share infomation.
-    shareInfo(title, html, url, message){
+    shareInfo(title, html, url, message) {
         const { ios, android } = window.currentDevice;
         ios && JS_ToShare(title, html, url, message);
         android && window.yudada.JS_ToShare(title, html, url, message);
     }
 
     //release voice info.
-    releaseVoiceInfo(){
+    releaseVoiceInfo() {
         const { ios, android } = window.currentDevice;
         ios && JS_RecordingModal();
         android && window.yudada.JS_RecordingModal();
     }
 
     //native alert style.
-    nativeAlert(){
+    nativeAlert() {
         const { ios, android } = window.currentDevice;
         ios && JS_ShowAlertWithTitles(title, message, button1, button2);
         android && window.yudada.JS_ShowAlertWithTitles(title, message, button1, button2);
     }
 
     //native login.
-    nativeLogin(username, code){
+    nativeLogin(username, code) {
         const { ios, android } = window.currentDevice;
         const obj = {
             'tele': username,
@@ -84,16 +86,32 @@ class CustomClass {
         android && window.yudada.JS_Login(obj.tele, obj.pass, null);
     }
 
-    getUserValue(key){
+    getUserValue(key) {
         const { ios, android } = window.currentDevice;
         const token = ios ? JS_PerferenceGetShared(key) : window.yudada.JS_Token();
         return token;
     }
 
-    logOut(){
+    logOut() {
         const { ios, android } = window.currentDevice;
         ios && JS_UserExitLog();
         android && window.yudada.JS_UserExitLog();
+    }
+
+    getAPi() {
+        const { ios, android } = window.currentDevice;
+        if (!window['JS_BaseUrl'] && (!window['yudada'] || !window['yudada']['JS_BaseUrl'])) {
+            return false;
+        }
+        return ios ? JS_BaseUrl() : window.yudada.JS_BaseUrl();
+    }
+
+    getDistricInfo() {
+        const { ios, android } = window.currentDevice;
+        if (!window['JS_DisTanceInfo'] && (!window['yudada'] || !window['yudada']['JS_DisTanceInfo'])) {
+            return false;
+        }
+        ios ? JS_DisTanceInfo() : window.yudada.JS_DisTanceInfo();
     }
 
 }
